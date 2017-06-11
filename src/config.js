2c07bd72e8de26d890e870f2a72805e73c289802
John Sylvain <jsylvain007@gmail.com>
2017-06-11 09:29:38 -0500

    removed level

--- src/config.js
+++ src/config.js
@@ -1,7 +1,5 @@
-import level from 'level';
 import NodeCache from 'node-cache';
 
 export const myCache = new NodeCache();
-export const db = level('../db');
 export const port = process.env.PORT || 1337;
 export const appName = 'PlaceMorty';
